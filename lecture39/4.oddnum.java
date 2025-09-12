7f06558d2a4e398d6d16bc971e10d3b71e721716
priyanshu69-a <priyanshurana5002@gmail.com>
2025-09-12 10:49:19 +0530

    Create 4.oddnum.java

---
+++ lecture39/4.oddnum.java
@@ -0,0 +1,11 @@
+package com.chitkara.day39;
+
+public class oddnum {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 2 != 0) { // check if the number is odd
+                System.out.println(i);
+            }
+        }
+    }
+}
